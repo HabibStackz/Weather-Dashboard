ee082369361294c1dec9c0f901cf02de35e44127
raplordrds <habib.hakeem@yahoo.com>
2024-01-19 23:03:05 +0000

    created a function to update html with today's weather data

--- starter/script.js
+++ starter/script.js
@@ -13,3 +13,37 @@ async function getWeatherData(city){
   }
 }
 
+// Function to fetch 5-day weather forecast based on latitude and longitude
+async function getWeatherForecast(lat, lon) {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching weather forecast:', error);
+    return null;
+  }
+}
+
+// Function to update html with current weather data
+function updateWeatherUI(weatherData){
+  const cityElement = document.getElementById('today').querySelector('h2');
+  const tempElement = document.getElementById('temp');
+  const windElement = document.getElementById('wind');
+  const humidityElement = document.getElementById('humidity');
+  const iconElement = document.getElementById('weather-icon');
+
+  if (weatherData) {
+    // update html with today's weather data
+    cityElement.textContent = weatherData.name;
+    tempElement.textContent = `Temp: ${weatherData.main.temp} °C`;
+    windElement.textContent = `Wind: ${weatherData.wind.speed} M/S`;
+    humidityElement.textContent = `Humidity: ${weatherData.main.humidity} %`
+
+    if (weatherData.weather && weatherData.weather[0] && weatherData.weather[0].icon) {
+      const iconUrl = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+      iconElement.src = iconUrl;
+      iconElement.alt = 'Weather Icon';
+    }
+  }
+}
